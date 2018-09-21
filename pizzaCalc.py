c15e63008d0759b81c8da43c8bfba402043fa087
Mats <99052518@mydavinci.nl>
2018-09-21 12:06:51 +0200

    Added main

---
+++ pizzaCalc.py
@@ -0,0 +1,26 @@
+small = 0
+medium = 0
+large = 0
+Xsmall = 0
+Xmedium = 0
+Xlarge = 0
+PRICESMALL = 3.99
+PRICEMEDUIM = 5.99
+PRICELARGE = 9.00·
+totalSmall = 0.00
+totalMedium = 0.00
+totalLarge = 0.00
+total = 0.00
+
+print('Welke pizza wilt je?')
+print('[1] - Small')
+print('[2] - Medium')
+print('[3] - Large\n')
+
+size = raw_input();
+if size == '1':
+    print('Hoeveel wil je er?')
+    Xsmall = raw_input()
+    small =+ Xsmall
+    print(small)
+    pass
